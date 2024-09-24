489d72b387bf540b9b4b583b226a76cfaac7d3e2
benjaminfrias <benjaminfrias2004@gmail.com>
2024-09-24 13:22:53 -0400

    add todo pop up

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import "./styles.css";
 import "./reset.css";
+import { createTodoFactory } from "./createTodo";
+
+const datePicker = document.getElementById("date-container");
+flatpickr(datePicker, { wrap: true });
