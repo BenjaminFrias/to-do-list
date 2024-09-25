d6efd02869387ce848d2d1d6677ed3cdfd34f589
benjaminfrias <benjaminfrias2004@gmail.com>
2024-09-25 17:14:41 -0400

    add close pop up button

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@ import "./styles.css";
 import "./reset.css";
 import { createTodoFactory } from "./createTodo";
 
-const datePicker = document.getElementById("date-container");
+const newTodoBtn = document.querySelector("#new-to-do-btn");
+const toDoPopUpContainer = document.querySelector("#to-do-pop-up-container");
+const closePopUpBtn = document.querySelector("#close-pop-up");
+
+// Toggle to do pop up div
+newTodoBtn.addEventListener("click", () => {
+	toDoPopUpContainer.classList.add("active");
+});
+
+// Close pop up button
+
+closePopUpBtn.addEventListener("click", () => {
+	toDoPopUpContainer.classList.remove("active");
+});
+
+// Date picker - flatpickr
+const datePicker = document.querySelector("#date-container");
 flatpickr(datePicker, { wrap: true });
